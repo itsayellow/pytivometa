b49b41fcf5a13c7472a69587c597f5cb6f7a7f25
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-19 14:47:09 -0700

    Fix bugs in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ HERE = Path(__file__).resolve()
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = ["IMDbPY>=6.5"]
 
 setup_requirements = []
 
@@ -22,7 +22,7 @@ setup(
     author_email="itsayellow+dev@gmail.com",
     python_requires=">=3.5",
     classifiers=[
-        "Development Status :: 3 Alpha",
+        "Development Status :: 3 Beta",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
@@ -31,19 +31,20 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     description="Creates metadata for pytivo corresponding to video files.",
-    entry_points={
-        "console_scripts": ["pytivometa = pytivometa.cli:pytivometa"]
-    },
+    entry_points={"console_scripts": ["pytivometa = pytivometa.cli:pytivometa"]},
     install_requires=requirements,
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
+    long_description=readme + "\n",
+    #include_package_data=True,
+    package_data = {
+        '' : ["*.pem"]
+    },
     keywords="pytivo",
     name="pytivometa",
     packages=find_packages(include=["pytivometa", "pytivometa.*"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    # url='https://github.com/itsayellow/pymclapp',
-    version="0.1.0",
+    # url='https://github.com/itsayellow/pytivometa',
+    version="0.2.0",
     zip_safe=False,
 )

e69283dbd8e1986266109fcf6281f2782726c376
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-22 16:53:29 -0800

    Remove commented out line.

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    # url='https://github.com/itsayellow/pytivometa',
     version="0.2.1",
     zip_safe=False,
 )
